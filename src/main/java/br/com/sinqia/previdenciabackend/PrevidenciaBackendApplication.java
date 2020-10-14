67672b1057a8a70be0c5407afb9335943687a51c
Wendell Modesto Silva <wendell.mod.silva@gmail.com>
2020-10-14 08:53:23 -0300

    first commit

---
+++ src/main/java/br/com/sinqia/previdenciabackend/PrevidenciaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.sinqia.previdenciabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevidenciaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevidenciaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sinqia/previdenciabackend/PrevidenciaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sinqia.previdenciabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevidenciaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
